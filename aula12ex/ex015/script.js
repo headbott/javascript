b9c076913c291aee6e4e23934eb8c390b7d167ac
headbott <126895101+headbott@users.noreply.github.com>
2023-05-23 11:54:42 -0300

    ex015
    terminado

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,3 +1,59 @@
 function verificar() {
-    window.alert('alert')
+    var data = new Date()
+    var ano = data.getFullYear() // ano com 4 digitos - ex: 2023
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) { // se o que tiver checado for o fsex 0
+            gênero = 'Homem'
+            if (idade >=0 && idade < 13) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 59) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 13) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 59) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img) // adicionar um elemento abaixo
+
+    }
 }
+
+/* 
+
+fano.value.length == 0 
+
+se o comprimento do valor é igual a 0 
+
+*/
+
