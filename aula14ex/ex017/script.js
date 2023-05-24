130e8132be704e9c3f694a165506fdf3df07aa93
headbott <126895101+headbott@users.noreply.github.com>
2023-05-24 16:22:41 -0300

    ex 017 feito

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = '' // antes de mostrar a tabuada (limpa a area da tabuada)
+        while (c <= 10) {
+            // adicion <option> no HTML (de forma dinâmica) //
+            let item = document.createElement('option') // option
+            item.text = `${n} x ${c} = ${n*c}` // value
+            item.value = `tab${c}` // ajuda a saber qual item foi selecionado - tab 1 2 3 (usado mais para outra linguagens)
+            tab.appendChild(item) // adicionar a tabuada
+            c++
+        }
+    }
+    
+}
